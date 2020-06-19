90556df5e17a3e7063e03ed47ae343c6112fd88e
Adriano R S Souza <silva.souza.adriano@gmail.com>
2020-06-19 20:01:41 -0300

    Created books by category page

--- src/pages/Category/index.tsx
+++ src/pages/Category/index.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
-import { useRouteMatch, Link } from 'react-router-dom';
-import { FiArrowLeft } from 'react-icons/fi';
+import React, { useState, useEffect, useCallback, ChangeEvent } from 'react';
+import { useRouteMatch, Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft, FiChevronRight } from 'react-icons/fi';
 
-import { Category } from './styles';
+import { Category, Container, Book } from './styles';
+
+interface Book {
+  id: string;
+  timestamp: Date;
+  title: string;
+  description: string;
+  author: string;
+  category: string;
+  deleted?: boolean;
+}
 
 interface BookCategoryParams {
   id: string;
@@ -11,17 +21,106 @@ interface BookCategoryParams {
 
 const BookListByCategory: React.FC = () => {
   const { params } = useRouteMatch<BookCategoryParams>();
+  const [books, setBooks] = useState<Book[]>(() => {
+    const storagedBooks = localStorage.getItem('@MyBooks:books');
+
+    if (storagedBooks) {
+      return JSON.parse(storagedBooks);
+    }
+    return [];
+  });
+
+  const bookCategoryId = params.id;
+  const history = useHistory();
+
+  useEffect(() => {
+    const newBookList: Book[] = books
+      .filter((book) => book.category === bookCategoryId && !book.deleted)
+      .sort(function compare(a, b) {
+        if (a.title < b.title) return -1;
+        if (a.title > b.title) return 1;
+        return 0;
+      });
+    setBooks(newBookList);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleBookSort = useCallback(
+    (event: ChangeEvent<HTMLSelectElement>) => {
+      const bookSortSelected = event.target.value;
+      if (bookSortSelected !== 'none') {
+        const newBookList: Book[] = books
+          .filter((book) => book.category === bookCategoryId && !book.deleted)
+          .sort(function compare(a, b) {
+            if (bookSortSelected === 'date') {
+              if (a.timestamp < b.timestamp) return -1;
+              if (a.timestamp > b.timestamp) return 1;
+            } else {
+              if (a.title < b.title) return -1;
+              if (a.title > b.title) return 1;
+            }
+            return 0;
+          });
+        setBooks(newBookList);
+      } else {
+        setBooks(books);
+      }
+    },
+    [books, bookCategoryId],
+  );
 
   return (
-    <Category>
-      <h1>
-        Category: <span>{params.title}</span>
-      </h1>
-      <Link to="/">
-        <FiArrowLeft />
-        Back to home
-      </Link>
-    </Category>
+    <>
+      <Category>
+        <main>
+          <strong>
+            Category: <span>{params.title}</span>
+          </strong>
+          <div />
+          <select
+            name="book-ordering"
+            id="book-ordering"
+            onChange={handleBookSort}
+          >
+            <option value="none">Select a ordering</option>
+            <option value="title">Title</option>
+            <option value="date">Date</option>
+          </select>
+
+          <Link to="/addbook">
+            <button type="button">Add Book</button>
+          </Link>
+        </main>
+
+        <aside>
+          <Link to="/">
+            <div>
+              <FiArrowLeft />
+              <span>Back to home</span>
+            </div>
+          </Link>
+        </aside>
+      </Category>
+
+      <Container>
+        {books.map((book) => (
+          <Link to="/viewdetailbook" key={book.id}>
+            <Book>
+              <main>
+                <strong>{book.title}</strong>
+                <span>{book.timestamp}</span>
+              </main>
+              <aside>
+                <p>{book.description}</p>
+                <div>
+                  <FiChevronRight />
+                </div>
+              </aside>
+            </Book>
+          </Link>
+        ))}
+      </Container>
+    </>
   );
 };
 
--- src/pages/Category/styles.ts
+++ src/pages/Category/styles.ts
@@ -2,96 +2,150 @@ import styled from 'styled-components';
 import { shade } from 'polished';
 
 export const Category = styled.div`
-  max-width: 65vw;
+  max-width: 100%;
   display: flex;
   align-items: center;
   justify-content: space-between;
   margin-top: 60px;
 
-  h1 span {
-    color: #3a3a3a;
-    font-weight: 100;
-  }
-  a {
+  main {
     display: flex;
     align-items: center;
 
-    color: #3a3a3a;
-    text-decoration: none;
-    margin-right: 16px;
-    transition: color 0.2s;
+    strong {
+      font-size: 2vw;
+      margin-right: 16px;
+    }
 
-    &:hover {
-      color: ${shade(0.2, '#04d361')};
+    strong span {
+      color: #3a3a3a;
+      font-weight: 100;
+    }
+
+    button {
+      width: 95px;
+      height: 35px;
+      background: #04d361;
+      border-radius: 8px;
+      border: 0;
+      color: #fff;
+      font-weight: bold;
+      font-size: 2.5vh;
+      transition: background-color 0.2s;
+      flex-wrap: wrap;
+
+      &:hover {
+        background: ${shade(0.2, '#04d361')};
+      }
+
+      @media (max-width: 768px) {
+        font-size: 2.5vh;
+      }
+
+      @media (max-width: 425px) {
+        width: 60px;
+        height: 30px;
+        font-size: 2vh;
+      }
+    }
+
+    select {
+      width: 130px;
+      height: 35px;
+      appearance: none;
+      background: #04d361;
+      border-radius: 8px;
+      border: 0;
+      color: #fff;
+      font-weight: bold;
+      padding: 10px;
+      margin-right: 16px;
+      transition: background-color 0.2s;
+
+      &:hover {
+        background: ${shade(0.2, '#04d361')};
+      }
+
+      @media (max-width: 425px) {
+        width: 90px;
+        height: 30px;
+        font-size: 2vh;
+      }
     }
   }
 
-  svg {
-    margin-right: 16px;
-    color: #04d361;
+  aside {
+    display: flex;
+    align-items: center;
+
+    a {
+      color: #3a3a3a;
+      text-decoration: none;
+      margin-right: 16px;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#04d361')};
+      }
+    }
+
+    div {
+      display: flex;
+      align-items: center;
+    }
   }
 `;
 
-export const Books = styled.div`
-  max-width: 63vw;
-  margin-top: 40px;
-
-  @media (max-width: 1024px) {
-    max-width: 72vw;
-  }
-
-  @media (max-width: 768px) {
-    max-width: 83vw;
-  }
-
+export const Container = styled.div`
+  max-width: 100%;
+  margin-top: 50px;
   a {
-    width: 100%;
-    height: 100px;
-    background: #fff;
-    border-radius: 5px;
-    display: block;
     text-decoration: none;
+  }
+`;
 
-    padding: 24px;
+export const Book = styled.div`
+  margin-top: 10px;
+  display: block;
+  overflow: hidden;
+  word-break: break-all;
+  max-height: 100px;
+  background-color: #fff;
+  border-radius: 5px;
+  padding: 10px;
+
+  main {
     display: flex;
     align-items: center;
-    transition: transform 0.2s;
+    justify-content: space-between;
 
-    &:hover {
-      transform: translateX(10px);
+    strong {
+      font-size: 1.5vw;
+      color: #3d3d4d;
+      margin-right: 10px;
     }
 
-    & + a {
-      margin-top: 16px;
+    span {
+      color: #a8a8b3;
     }
   }
 
-  div {
-    > div {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-
-      strong {
-        font-size: 1.5vw;
-        color: #3d3d4d;
-        margin-right: 10px;
-      }
-
-      span {
-        color: #a8a8b3;
-      }
-    }
+  aside {
+    margin-top: 5px;
+    display: flex;
+    justify-content: space-between;
 
     p {
+      align-items: left;
       font-size: 1.2vw;
       color: #a8a8b3;
       margin-top: 5px;
     }
-  }
-
-  svg {
-    margin-left: auto;
-    color: #cdcdb6;
+    svg {
+      margin-left: auto;
+      margin-right: -10px;
+      color: #cdcdb6;
+      font-size-adjust: 20px;
+    }
   }
 `;
